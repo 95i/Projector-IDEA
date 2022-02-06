1b712f75e339eeff25d3b2d804ecfaf1fd3919f8
95i <54938994+95i@users.noreply.github.com>
2022-02-06 11:42:12 +0800

    Update delay.sh

--- delay.sh
+++ delay.sh
@@ -1,10 +1,6 @@
 #!/bin/bash  
   
-step=10 #间隔的秒数，不能大于60  
-  
-for (( i = 0; i < 60; i=(i+step) )); do  
-    $(curl 'http://www.google.com/')  
-    sleep $step  
-done  
-  
-exit 0 
+while true
+do
+command;
+done
